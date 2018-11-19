d124b418f74f1083544a9b87daf9a0d02a59bb89
hyperpineapple <linards.e.s@inbox.lv>
2018-11-19 21:38:07 +0200

    BotThrow

---
+++ Mayo Man's Adventures/Assets/Scripts/BotThrow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotThrow : MonoBehaviour {
+
+    public Transform player;
+    public float delay = 3f;
+    public GameObject Granade;
+    float cowntDown;
+    Vector3 off;
+    private float ForceX;
+    private float ForceZ;
+    public float ForceY;
+
+    // Use this for initialization
+    void Start()
+    {
+        cowntDown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cowntDown -= Time.deltaTime;
+        if (cowntDown <= 0f)
+        {
+            Explode();
+            cowntDown = delay;
+        }
+    }
+
+    void Explode()
+    {
+        off = player.position - transform.position;
+        ForceX = off.x;
+        ForceZ = off.z;
+        GameObject granade = Instantiate(Granade, transform.position, transform.rotation);
+        Rigidbody rb = granade.GetComponent<Rigidbody>();
+        rb.AddForce(ForceX, ForceY, ForceZ, ForceMode.Impulse);
+
+    }
+}
--- Mayo Man's Adventures/Assets/Scripts/PlayerThrow.cs
+++ Mayo Man's Adventures/Assets/Scripts/PlayerThrow.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Throw : MonoBehaviour {
+public class PlayerThrow : MonoBehaviour {
 
     public float Force = 20f;
     public GameObject Granade;
