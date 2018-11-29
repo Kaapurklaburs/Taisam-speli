08e0e18efebf5483e4a2cb5ca204030732247cd2
hyperpineapple <linards.e.s@inbox.lv>
2018-11-29 21:33:00 +0200

    MiniMap

---
+++ Mayo Man's Adventures/Assets/Scripts/MinimapCam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCam : MonoBehaviour {
+
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosision = player.position;
+        newPosision.y = transform.position.y;
+        transform.position = newPosision;
+    }
+
+}
