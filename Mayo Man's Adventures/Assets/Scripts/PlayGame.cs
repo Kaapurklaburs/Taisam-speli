86827c67517c2958980afe790e21b2a555c9cb89
hyperpineapple <linards.e.s@inbox.lv>
2018-12-01 22:28:21 +0200

    Menu uzlabojumi

--- Mayo Man's Adventures/Assets/Scripts/PlayGame.cs
+++ Mayo Man's Adventures/Assets/Scripts/PlayGame.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class PlayGame : MonoBehaviour {
 
-    public void LoadGame()
+    public void LoadGame1()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadGame2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void LoadGame3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
     }
 
     public void QuitGame()
