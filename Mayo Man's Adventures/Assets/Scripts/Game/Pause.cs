26cc3cc6e59816397cfdaa7da57f4ad49853711b
hyperpineapple <linards.e.s@inbox.lv>
2018-12-05 21:40:42 +0200

    pause

---
+++ Mayo Man's Adventures/Assets/Scripts/Game/Pause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public static bool IsPaused = false;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+        }
+	}
+    void Resume ()
+    {
+
+    }
+
+    void Pause ()
+    {
+
+    }
+}
