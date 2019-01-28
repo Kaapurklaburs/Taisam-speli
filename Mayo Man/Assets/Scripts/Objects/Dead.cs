0ebc3fa2c5738d200c0248483cf1a3e6582b217d
hyperpineapple <44949313+hyperpineapple@users.noreply.github.com>
2019-01-28 15:44:43 +0200

    Fakapoju hitbox

---
+++ Mayo Man/Assets/Scripts/Objects/Dead.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dead : MonoBehaviour {
+
+    public Rigidbody BodyPart;
+    public float Force;
+
+	// Use this for initialization
+	void Start () {
+        BodyPart.AddForce(transform.forward * Force, ForceMode.Impulse);
+
+    }
+
+}
