3f35625bfe7b8d93662744b9ca7d0a6ce6694207
hyperpineapple <44949313+hyperpineapple@users.noreply.github.com>
2019-02-16 11:24:41 +0200

    pirmais esteregg

---
+++ Mayo Man/Assets/Scripts/Game/EsterEggs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EsterEggs : MonoBehaviour {
+
+    public float time = 0.5f;
+
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (Input.GetKey("t"))
+        {
+            Time.timeScale = time;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+	}
+}
