80c68304ad905981532eb20ee503d691ec4dbdd7
hyperpineapple <linards.e.s@inbox.lv>
2019-03-17 11:22:51 +0200

    /

---
+++ Mayo Man/Assets/Scripts/Objects/Car.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    public HingeJoint weell1;
+    public HingeJoint weell2;
+    public HingeJoint weell3;
+    public HingeJoint weell4;
+
+    private bool inside;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inside = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inside = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        weell1.useMotor = false;
+        weell2.useMotor = false;
+        weell3.useMotor = false;
+        weell4.useMotor = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        weell1.useMotor = inside;
+        weell2.useMotor = inside;
+        weell3.useMotor = inside;
+        weell4.useMotor = inside;
+    }
+}
