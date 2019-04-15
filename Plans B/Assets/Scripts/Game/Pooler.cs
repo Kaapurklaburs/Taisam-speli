3929ab0750008811207489f7f4e0370118e6d1ea
hyperpineapple <44949313+hyperpineapple@users.noreply.github.com>
2019-04-15 22:18:09 +0300

    pooler

---
+++ Plans B/Assets/Scripts/Game/Pooler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string Tag;
+        public GameObject Prefab;
+        public int Size;
+    }
+
+    public static Pooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    void Start()
+    {
+
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.Size; i++)
+            {
+                GameObject obj = Instantiate(pool.Prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            poolDictionary.Add(pool.Tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.Log("Pool with tag" + tag + "dosn't exist");
+            return null;
+        }
+
+
+        GameObject ObjectToSpawn = poolDictionary[tag].Dequeue();
+
+        ObjectToSpawn.SetActive(true);
+        ObjectToSpawn.transform.position = position;
+        ObjectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(ObjectToSpawn);
+
+        return ObjectToSpawn;
+    }
+}
---
+++ Plans B/Assets/Scripts/Objects/PoolSpawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolSpawn : MonoBehaviour
+{
+    public string Pool;
+
+    private void FixedUpdate()
+    {
+        Pooler.Instance.SpawnFromPool(Pool, transform.position, Quaternion.identity);
+    }
+}
