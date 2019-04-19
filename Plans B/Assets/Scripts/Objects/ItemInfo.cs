89730a24bbf49a3e25fa14a8c043d30492dcce7a
Diamondtroller <46786192+Diamondtroller@users.noreply.github.com>
2019-04-19 12:05:34 +0300

    Soma+cuhna
    Ieliku kaut kādu somu vajag tikai ielikt grafisko daļu

---
+++ Plans B/Assets/Scripts/Objects/ItemInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInfo : MonoBehaviour
+{
+    [SerializeField]
+    public Structs.Item Item;
+
+}
