4ee47356a8651eaefbb465bd010f2f28658e9857
hyperpineapple <44949313+hyperpineapple@users.noreply.github.com>
2019-04-29 16:03:33 +0300

    epic fuckup

---
+++ Plans B/Assets/Scripts/Objects/ActiveRagdoll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveRagdoll : MonoBehaviour
+{
+    Vector3 Position;
+    public float antiG;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Position = transform.position + new Vector3(0f, 1f, 0f);
+        Vector3 posionS = Vector3.Lerp(transform.position, Position, antiG);
+        transform.position = posionS;
+    }
+}
