bdc556887326771be7ba794abf1dead0595a990f
hyperpineapple <44949313+hyperpineapple@users.noreply.github.com>
2019-05-07 22:23:47 +0300

    menu un citi uzlabojumi

---
+++ Plans B/Assets/Scripts/Game/HPbar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPbar : MonoBehaviour
+{
+    public AllPlayerStats ps;
+    public Image hpBar;
+    private float hp1;
+    private float hp2;
+
+
+
+    public float amount { get; internal set; }
+
+    private void Start()
+    {
+        hp1 = ps.PlayerStats.Health;
+    }
+
+    void Update()
+    {
+        hp2 = ps.PlayerStats.Health;
+        hpBar.fillAmount = hp2 / hp1;
+    }
+}
---
+++ Plans B/Assets/Scripts/Game/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void LoadGame1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1f;
+
+    }
+    public void LoadGame2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        Time.timeScale = 1f;
+    }
+    public void LoadGame3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+    /*
+    public void SetSlider(float Sensativity)
+    {
+        PlayerLook.sensativity = Sensativity;
+    }
+    */
+}
